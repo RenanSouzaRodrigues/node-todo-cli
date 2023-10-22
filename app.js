9dc5885e5a65aec3f1d761b23ac396e9a05fc422
Renan Souza <renan.souza@powerpush.com.br>
2023-10-21 22:39:47 -0300

    small fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
-const { resolve, dirname, join } = require("path");
+const { dirname, join } = require("path");
 const { program } = require("commander");
 
 const getDB = () => {
